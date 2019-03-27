1a3113cd978816c0baed59182dad462cd3000a7f
Júlio Hidemi Kamizato <jkamizato@gmail.com>
2019-03-26 22:28:24 -0300

    Commit vuetify

---
+++ src/components/Product.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="product">
+        <div class="product-detail">
+        {{ product }}
+        </div>
+
+        <form @submit.prevent="submitProduct(product)">
+            <input v-model="product" />
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Product",
+        data() {
+            return {
+                product: "Socks"
+            }
+        },
+        methods: {
+            submitProduct(product) {
+                alert (product);
+            }
+       }
+    }
+</script>
+
+<style scoped>
+
+</style>
